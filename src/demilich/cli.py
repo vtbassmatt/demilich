bc7270ad8e6543780885e63c3e2554ac74294a94
Matt Cooper <vtbassmatt@gmail.com>
2024-12-28 08:34:46 -0500

    allow for suppressing longer items in the skeleton

--- src/demilich/cli.py
+++ src/demilich/cli.py
@@ -19,16 +19,30 @@ def play_booster(
     format: Annotated[
         OutputFormat, typer.Argument()
     ] = OutputFormat.csv,
+    include_instruction: Annotated[
+        bool|None, typer.Option(help="Include skeleton instruction")
+    ] = None,
+    include_text: Annotated[
+        bool|None, typer.Option(help="Include card text")
+    ] = None,
 ):
     """
     Generate a standard play booster skeleton.
     """
     from demilich.examples.play_booster_2024 import pb2024
+
+    all_fields = ['id', 'instruction', 'name', 'cost', 'typeline', 'text', 'stats']
+    fields = {field: True for field in all_fields}
+
     if format == OutputFormat.csv:
-        write_csv(pb2024(), ['id', 'instruction', 'name', 'cost', 'typeline', 'text', 'stats'])
+        fields['instruction'] = False if include_instruction is False else True
+        fields['text'] = False if include_text is False else True
+        write_csv(pb2024(), [key for key, value in fields.items() if value])
 
     elif format == OutputFormat.table:
-        write_table(pb2024(), ['id', 'name', 'cost', 'typeline', 'stats', 'text'])
+        fields['instruction'] = True if include_instruction is True else False
+        fields['text'] = False if include_text is False else True
+        write_table(pb2024(), [key for key, value in fields.items() if value])
 
     else:
         raise NotImplementedError(format)
