e067f9bad243ddbb3fc924072558d2b3de10bcdc
Matt Cooper <vtbassmatt@gmail.com>
2024-12-29 14:48:31 -0500

    add custom TOML support to the CLI

--- src/demilich/cli.py
+++ src/demilich/cli.py
@@ -1,9 +1,14 @@
 from enum import Enum
+from pathlib import Path
 
 import typer
 from typing_extensions import Annotated
 
-from demilich.reader import generate_skeleton, load_from_resources
+from demilich.reader import (
+    generate_skeleton,
+    load_from_resources,
+    load_from_file,
+)
 from demilich.output import write_csv, write_table
 
 
@@ -31,7 +36,32 @@ def play_booster(
     Generate a standard play booster skeleton.
     """
     data = load_from_resources('pb2024.toml')
+    _generate(data, format, include_instruction, include_text)
 
+
+@app.command()
+def custom_skeleton(
+    filename: Annotated[
+        Path, typer.Argument()
+    ],
+    format: Annotated[
+        OutputFormat, typer.Argument()
+    ] = OutputFormat.csv,
+    include_instruction: Annotated[
+        bool|None, typer.Option(help="Include skeleton instruction")
+    ] = None,
+    include_text: Annotated[
+        bool|None, typer.Option(help="Include card text")
+    ] = None,
+):
+    """
+    Generate a skeleton from a TOML file.
+    """
+    data = load_from_file(filename)
+    _generate(data, format, include_instruction, include_text)
+
+
+def _generate(data, format, include_instruction, include_text):
     all_fields = ['id', 'instruction', 'name', 'cost', 'typeline', 'text', 'stats']
     fields = {field: True for field in all_fields}
 
@@ -49,10 +79,12 @@ def play_booster(
         raise NotImplementedError(format)
 
 
-@app.command()
-def dev():
-    """
-    Generate a partial skeleton, exercising a few features.
-    """
-    from demilich.examples.dev import dev_skeleton
-    write_csv(dev_skeleton(), ['id', 'instruction', 'name', 'cost', 'typeline', 'text', 'stats'])
+# Left for demonstration purposes - this exercises the example
+# in examples/dev.py
+# @app.command()
+# def dev():
+#     """
+#     Generate a partial skeleton, exercising a few features.
+#     """
+#     from demilich.examples.dev import dev_skeleton
+#     write_csv(dev_skeleton(), ['id', 'instruction', 'name', 'cost', 'typeline', 'text', 'stats'])
