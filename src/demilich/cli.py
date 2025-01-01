3cdc5f72b7d47f9b2fd690d46b6cb094ae69931f
Matt Cooper <vtbassmatt@gmail.com>
2025-01-01 10:01:34 -0500

    optionally generate limited chunks of the skeleton

--- src/demilich/cli.py
+++ src/demilich/cli.py
@@ -8,12 +8,19 @@ from demilich.reader import (
     generate_skeleton,
     load_from_resources,
     load_from_file,
+    FRAMES,
+    RARITIES,
 )
 from demilich.output import write_csv, write_table
 
 
 ALL_FIELDS = ['id', 'instruction', 'name', 'cost', 'typeline', 'stats', 'text']
 FIELDS_HELP = f"Comma-separated fields to include in the output from among: {','.join(ALL_FIELDS)}"
+FIELDS_METAVAR = "field_list"
+RARITIES_HELP = "Which rarities to include in output (C, U, R, M)"
+RARITIES_METAVAR = "rarities"
+FRAMES_HELP = "Which frames to include in output (W, U, B, R, G, A, Z, L)"
+FRAMES_METAVAR = "frames"
 
 class OutputFormat(str, Enum):
     csv = "csv"
@@ -29,15 +36,20 @@ def play_booster(
         OutputFormat, typer.Option()
     ] = OutputFormat.csv,
     fields: Annotated[
-        str, typer.Option(help=FIELDS_HELP, metavar="field_list")
+        str, typer.Option(help=FIELDS_HELP, metavar=FIELDS_METAVAR)
     ] = "",
+    rarities: Annotated[
+        str, typer.Option(help=RARITIES_HELP, metavar=RARITIES_METAVAR)
+    ] = "CURM",
+    frames: Annotated[
+        str, typer.Option(help=FRAMES_HELP, metavar=FRAMES_METAVAR)
+    ] = "WUBRGAZL",
 ):
     """
     Generate a standard play booster skeleton.
     """
-    fields = _get_fields(fields, format)
-    data = load_from_resources('pb2024.toml')
-    _generate(data, format, fields)
+    data_loader = lambda: load_from_resources('pb2024.toml')
+    _generate(data_loader, format, fields, rarities, frames)
 
 
 @app.command()
@@ -49,15 +61,38 @@ def custom_skeleton(
         OutputFormat, typer.Option()
     ] = OutputFormat.csv,
     fields: Annotated[
-        str, typer.Option(help=FIELDS_HELP, metavar="field_list")
+        str, typer.Option(help=FIELDS_HELP, metavar=FIELDS_METAVAR)
     ] = "",
+    rarities: Annotated[
+        str, typer.Option(help=RARITIES_HELP, metavar=RARITIES_METAVAR)
+    ] = "CURM",
+    frames: Annotated[
+        str, typer.Option(help=FRAMES_HELP, metavar=FRAMES_METAVAR)
+    ] = "WUBRGAZL",
 ):
     """
     Generate a skeleton from a TOML file.
     """
-    fields = _get_fields(fields, format)
-    data = load_from_file(filename)
-    _generate(data, format, fields)
+    data_loader = load_from_file(filename)
+    _generate(data_loader, format, fields, rarities, frames)
+
+
+def _clean_rarities_and_frames(rarities, frames):
+    rarities = rarities.upper().replace(',', '')
+    set_rarities = set([x[0] for x in RARITIES])
+    for r in rarities:
+        if r not in set_rarities:
+            raise ValueError(f"unknown rarity {r} requested; "
+                             f"valid options are {[x[0] for x in RARITIES]}")
+    
+    frames = frames.upper().replace(',', '')
+    set_frames = set([x[0] for x in FRAMES])
+    for f in frames:
+        if f not in set_frames:
+            raise ValueError(f"unknown frame {f} requested; "
+                             f"valid options are {[x[0] for x in FRAMES]}")
+
+    return rarities, frames
 
 
 def _get_fields(field_str: str|None, format: OutputFormat):
@@ -77,12 +112,16 @@ def _get_fields(field_str: str|None, format: OutputFormat):
     return shown_fields
 
 
-def _generate(data, format, fields):
+def _generate(data_loader, format, fields, rarities, frames):
+    rarities, frames = _clean_rarities_and_frames(rarities.upper(), frames.upper())
+    fields = _get_fields(fields, format)
+    data = data_loader()
+
     if format == OutputFormat.csv:
-        write_csv(generate_skeleton(data), fields)
+        write_csv(generate_skeleton(data, rarities, frames), fields)
 
     elif format == OutputFormat.table:
-        write_table(generate_skeleton(data), fields)
+        write_table(generate_skeleton(data, rarities, frames), fields)
 
     else:
         raise NotImplementedError(format)
